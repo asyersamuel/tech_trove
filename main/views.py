98131a273ec5c43a6bdb41cb37224a182cbe7518
asyersamuel <asyer.marpaung@gmail.com>
2024-09-09 19:22:11 +0700

    update README.md

--- main/views.py
+++ main/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 
 def show_main(request):
+    products = [
+        {
+            "name": "Samsung S9+",
+            "price": 9599000,
+            "description": "Produk Bagus ini"
+        },
+        {
+            "name": "Samsung S8",
+            "price": 8999000,
+            "description": "Produk Murah ini"
+        },
+        {
+            "name": "Samsung S7",
+            "price": 7999000,
+            "description": "Produk Murah ini"
+        }
+    ]
+
     context = {
-        "name": "Samsung S9+",
-        "price": 9599000,
-        "description": "Produk Bagus ini"
+        "products": products
     }
 
     return render(request, "main.html", context)
