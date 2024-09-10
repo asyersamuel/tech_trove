bdd20d8df0e18d18d7661588004a62a63115fde2
asyersamuel <asyer.marpaung@gmail.com>
2024-09-10 08:02:07 +0700

    update HTML

--- main/views.py
+++ main/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
 
 def show_main(request):
-    products = [
-        {
-            "name": "Samsung S9+",
-            "price": 9599000,
-            "description": "Produk Bagus ini"
-        },
-        {
-            "name": "Samsung S8",
-            "price": 8999000,
-            "description": "Produk Murah ini"
-        },
-        {
-            "name": "Samsung S7",
-            "price": 7999000,
-            "description": "Produk Murah ini"
-        }
-    ]
-
     context = {
-        "products": products
+        "store_name" : "Tech Trove",
+        "name" : "Asyer Samuel Marpaung",
+        "class" : "PBP A"
     }
 
     return render(request, "main.html", context)
